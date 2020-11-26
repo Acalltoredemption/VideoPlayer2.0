60b42aad6661c7a4d8ebcb4555b56733800998ea
Acalltoredemption <acalltoredemption@gmail.com>
2020-11-25 16:21:31 -0800

    set up axios get request functionality to communicate with youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,6 +8,12 @@ class SearchBar extends React.Component {
         this.setState({term: event.target.value});
     };
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onFormSubmit(this.state.term);
+        // TODO: Make sure we call callback from parent component
+    };
+
 
 
     render(){
